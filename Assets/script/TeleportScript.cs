d9306666ddd24df85277620672cde2ec1f7a8b49
Rasmus <rasmus.helgesson@elev.ga.lbs.se>
2018-11-27 10:50:20 +0100

    Made another level to my game

---
+++ Assets/script/TeleportScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    public int amount = 101;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Player")
+        {
+            collision.transform.position = new Vector2(66.7587f, -21.38975f);
+            Money.score += amount;
+            Debug.Log("Teleport enter");
+
+        }
+
+    }
+
+}
+
+
+
